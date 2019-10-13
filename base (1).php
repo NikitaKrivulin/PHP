2d66e55ad8f52fe23c185f35fa6addc0a31801c7
NikitaKrivulin <39853243+NikitaKrivulin@users.noreply.github.com>
2019-10-13 19:01:17 +0300

    Add files via upload

---
+++ base (1).php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Export to PHP Array plugin for PHPMyAdmin
+ * @version 0.2b
+ */
+
+//
+// Database `test`
+//
+
+// `test`.`base`
+$base = array(
+  array('Number' => '1','Id' => '10','Name1' => 'Первый','Name2' => 'Третий','Name3' => 'Пятый'),
+  array('Number' => '2','Id' => '11','Name1' => 'Второй ','Name2' => 'Четвёртый','Name3' => 'Шестой')
+);
